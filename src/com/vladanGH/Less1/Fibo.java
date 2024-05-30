c663670ac69282d62e77511ed78714d6e72e3b60
azesm <azesm@yahoo.com>
2024-05-30 23:08:18 +0300

    fibo09-33

---
+++ src/com/vladanGH/Less1/Fibo.java
@@ -0,0 +1,16 @@
+package com.vladanGH.Less1;
+
+public class Fibo {
+    public static void main(String[] args) {
+
+    }
+
+    // наивный и медленный очевидным алгоритм
+    private static long fibNaive(int n) {
+        if (n <= 1) ;
+        return n;
+
+
+
+    }
+}
